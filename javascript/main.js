aecdad0e9c9b87a5be1a439508c00fc69f5a5994
Lee73C <lee.salinas88@outlook.com>
2025-01-08 10:28:08 -0300

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,10 +1,10 @@
-const isResumenCompraPage = window.location.href.includes("javascript/pages/resumenCompra.html");
-const isIndexPage = window.location.href.includes("javascript/index.html");
+const isResumenCompraPage = window.location.href.includes("/javascript/pages/resumenCompra.html");
+const isIndexPage = window.location.href.includes("/javascript/index.html");
 
 let productos = [];
 
 // Cargar los productos y recetas desde un archivo JSON
-fetch('/productos.json')
+fetch('/javascript/productos.json')
     .then(response => response.json())
     .then(data => {
         productos = data;
@@ -15,7 +15,7 @@ fetch('/productos.json')
     .catch(error => console.error('Error al cargar productos:', error));
 
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('/recetas.json')
+    fetch('/javascript/recetas.json')
         .then(response => response.json())
         .then(data => {
             window.recetas = data;
